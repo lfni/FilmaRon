3108c2113fc747913122acbfbc5c627c0e906086
LFnighTT <ltfyamyry00@gmail.com>
2025-09-02 03:50:23 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'db' => ['host'=>'127.0.0.1','port'=>3306,'user'=>'root','pass'=>'','name'=>'filmaron'],
+  'db' => ['host'=>'sql12.freesqldatabase.com','port'=>3306,'user'=>'sql12796998','pass'=>'hnInGFmWyE','name'=>'sql12796998'],
   'geo' => ['provider'=>'ipapi','timeout'=>2],
-  'app' => ['base_url'=>'/','name_fa'=>'دراما دی‌ال','logo_text'=>'DramaDL']
+  'app' => ['base_url'=>'/','name_fa'=>'فیلمارون','logo_text'=>'FilmaRon']
 ];
